f56027e738467bff3130b92cd45025efccb2059e
rhiannon bamford <rhiannon.bamford@gmail.com>
2021-02-28 16:46:39 +0000

    Basic program outline

---
+++ dhondt panic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace dhondt_panic
+{
+    /// <summary>
+    /// Program
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        /// <param name="args">arguments</param>
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.Error.WriteLine("File path required");
+                return;
+            }
+
+            Console.WriteLine($"Path = {args[0]}");          
+        }
+    }
+}
